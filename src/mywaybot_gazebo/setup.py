efff56380daf81c4060064ff5bd53123a8dbcfc3
HyuninKim <kimhyuninnnn@gmail.com>
2025-01-08 22:53:52 +0900

    rviz added

--- src/mywaybot_gazebo/setup.py
+++ src/mywaybot_gazebo/setup.py
@@ -12,14 +12,16 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        
+
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'models/dae'), glob('models/dae/*.dae')),
         (os.path.join('share', package_name, 'models/stl'), glob('models/stl/*.stl')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),
         (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
-        
+        (os.path.join('share', package_name, 'config'), glob('config/*'))
+
+
 
     ],
     extras_require={
